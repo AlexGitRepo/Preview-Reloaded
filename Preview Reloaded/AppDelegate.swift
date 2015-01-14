d5293562d140aa46c3358d65a7acc622e3a998cd
Alex S <alexgitrepo@users.noreply.github.com>
2015-01-13 19:19:50 -0500

    Initial Commit

---
+++ Preview Reloaded/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Preview Reloaded
+//
+//  Created by Alex Scardigno on 1/13/15.
+//  Copyright (c) 2015 zzz studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
